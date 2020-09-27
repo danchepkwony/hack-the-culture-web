7f8759146576866ebf5d9b78f869887bfa42ab96
danchepkwony <dan.k.chepkwony@gmail.com>
2020-09-27 16:43:03 -0400

    Updates

---
+++ precache-manifest.5a6e8643473b5af75401b36092ec225c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eee7e7c5c465177be90f92d91bbf9124",
+    "url": "/hack-the-culture-web/index.html"
+  },
+  {
+    "revision": "006ead2b2c4436252093",
+    "url": "/hack-the-culture-web/static/css/main.bd1eeac9.chunk.css"
+  },
+  {
+    "revision": "39d33cfffa498f133101",
+    "url": "/hack-the-culture-web/static/js/2.c54ce975.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hack-the-culture-web/static/js/2.c54ce975.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "006ead2b2c4436252093",
+    "url": "/hack-the-culture-web/static/js/main.8f09044e.chunk.js"
+  },
+  {
+    "revision": "09fed6f9ac7a12563711",
+    "url": "/hack-the-culture-web/static/js/runtime-main.c40ad0dc.js"
+  }
+]);
