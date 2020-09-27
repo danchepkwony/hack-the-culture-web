53719ffa65dcb4ab500d4b5e741a190d275ee2a0
Dan Chepkwony <67590846+danchepkwony@users.noreply.github.com>
2020-09-27 14:16:25 -0400

    initial commit

---
+++ src/components/Messenger/Messenger.js
@@ -0,0 +1,39 @@
+import React, {useState} from 'react';
+import './Messenger.css';
+
+const InitialState = {
+  text: "",
+  timestamp: "",
+  user_id: ""
+}
+
+function Messenger() {
+    const [messages, setMessages] = useState([
+      localStorage.getItem("messages") ? JSON.parse(localStorage.getItem("messages")) : InitialState]);
+
+    const onSend = () =>
+    { 
+        let message = document.getElementById('message_input').value;
+        let currentMessages = messages;
+        currentMessages.push({text: message, timestamp: '', user_id: "111"});
+        setMessages(currentMessages);
+        localStorage.setItem('messages', JSON.stringify(currentMessages));
+    }
+
+  return (
+    <div className="Messenger">
+      <div className="Messenger-messages">{messages.map((message, index) =>{
+          return (
+          <div key = {index} className = "Messenger-messages-text">
+            <div>{message.text}</div>
+          </div>)
+      })}</div> 
+      <form className ="Messenger-input">
+        <input type="text" id="message_input" className = "Messenger-input-text" />
+        <button className = "Messenger-input-button"  onClick={onSend}>Send</button>
+      </form>
+    </div>
+  );
+}
+
+export default Messenger;
